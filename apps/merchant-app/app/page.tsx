c4822dc3b8038062abd63258881fafbab566c4bd
Arjit31 <apk20023110@gmail.com>
2025-04-17 12:47:00 +0530

    initialize project with turborepo and add prisma and tailwind

---
+++ apps/merchant-app/app/page.tsx
@@ -0,0 +1,7 @@
+import { TestComponent2 } from "@repo/ui/testComponent2";
+
+export default function Home() {
+  return (
+    <TestComponent2></TestComponent2>
+  );
+}
---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,10 @@
+import {TestComponent} from "@repo/ui/testComponent"
+
+export default function Home() {
+  return (
+    <div>
+    <TestComponent></TestComponent>
+    <TestComponent></TestComponent>
+    </div>
+  );
+}
