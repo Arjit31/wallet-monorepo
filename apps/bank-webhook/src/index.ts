bfea377c3573a1c6b7ce52d9b57ac580f466d626
Arjit31 <apk20023110@gmail.com>
2025-05-04 17:03:34 +0530

    server working test

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -7,6 +7,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.json("server working!");
+})
+
 app.post("/bank-webhook", async function (req, res) {
   const paymentDetails: {
     token: string;
