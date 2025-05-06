5bd057a06b886ac2ffaa4cafb20268c09cfaf245
Arjit31 <apk20023110@gmail.com>
2025-05-06 13:15:40 +0530

    minor changes

--- apps/bank-webhook/src/queues/expireQueue.ts
+++ apps/bank-webhook/src/queues/expireQueue.ts
@@ -1,12 +1,11 @@
 import { Queue } from "bullmq";
 import IORedis from "ioredis";
 
-const connection = new IORedis({
-  host: "redis-18417.c15.us-east-1-4.ec2.redns.redis-cloud.com",
-  port: 18417,
-  password: "fH5M25ALwXKN95KL5zhCyvJoHGobhe28",
-  maxRetriesPerRequest: null,
-});
+const connection = new IORedis({ 
+  host: process.env.REDIS_HOST,
+  port: Number(process.env.REDIS_PORT),
+  password: process.env.REDIS_PASSWORD,
+  maxRetriesPerRequest: null });
 
 
 export const expireQueue = new Queue('expireQueue', { connection });
